18cd10119f9f52ec83db3d6ca9193f30c76090cb
Yuan <yuan@Yuans-MacBook-Pro-3.local>
2017-09-30 21:04:05 -0700

    final submission

--- client/src/quickaccess/QuickAccess.js
+++ client/src/quickaccess/QuickAccess.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Grid, Row, Col } from 'react-bootstrap';
 
 
 import "./QuickAccess.css";
@@ -54,46 +55,69 @@ class WebsiteList extends React.Component {
 		return(
 			<div>
 
-			<div className="item">
-				<img className="logo" src="https://facebook.github.io/react/img/logo_og.png" alt={this.props.links[0].name} />
-				<div className="weblink">
-				<h3><a href={this.props.links[0].url} target="_blank">{this.props.links[0].name}</a></h3>
-				<h4><span className="label label-success">Web</span></h4>
-				</div>
-			</div>
+			    <Row className="show-grid">
+			      <Col md={4}>
+			      	<div className="item">
+						<img className="logo" src="https://cdn-images-1.medium.com/max/675/1*oi8WLwC2u0EEI1j9uKmwWg.png" alt={this.props.links[0].name} />
+						<div className="weblink">
+							<h3><a href={this.props.links[0].url} target="_blank">{this.props.links[0].name}</a></h3>
+							<h4><span className="label label-success">Web</span></h4>
+						</div>
+					</div>
+			      </Col>
 
-			<div className="item">
-				<img className="logo" src="https://a0.muscache.com/airbnb/static/logos/belo-1200x630-a0d52af6aba9463c82017da13912f19f.png" alt={this.props.links[1].name} />
-				<div className="weblink">
-				<h3><a href={this.props.links[1].url} target="_blank">{this.props.links[1].name}</a></h3>
-				<h4><span className="label label-info">Travel</span></h4>
-				</div>
-			</div>
+			      <Col md={4}>
+			      	<div className="item">
+						<img className="logo" src="http://kintudesigns.com/wp-content/uploads/2016/11/angularjs.jpg" alt="angularjs" />
+						<div className="weblink">
+							<h3><a href="https://angularjs.org/" target="_blank">Angular</a></h3>
+							<h4><span className="label label-success">Web</span></h4>
+						</div>
+					</div>
+			      </Col>
 
-			<div className="item">
-				<img className="logo" src="http://i.cdn.cnn.com/cnn/.e/img/3.0/global/misc/cnn-logo.png" alt={this.props.links[2].name} />
-				<div className="weblink">
-				<h3><a href={this.props.links[2].url} target="_blank">{this.props.links[2].name}</a></h3>
-				<h4><span className="label label-warning">News</span></h4>
-				</div>
-			</div>
+			    </Row>
 
-			<div className="item">
-				<img className="logo" src="http://i0.kym-cdn.com/entries/icons/original/000/004/562/maxresdefault.jpg" alt={this.props.links[3].name} />
-				<div className="weblink">
-				<h3><a href={this.props.links[3].url} target="_blank">{this.props.links[3].name}</a></h3>
-				<h4><span className="label label-danger">Fun</span></h4>
-				</div>
-			</div>
+			    <Row className="show-grid">
+			      <Col md={4}>
+			      	<div className="item">
+						<img className="logo" src="https://a0.muscache.com/airbnb/static/logos/belo-1200x630-a0d52af6aba9463c82017da13912f19f.png" alt={this.props.links[1].name} />
+						<div className="weblink">
+							<h3><a href={this.props.links[1].url} target="_blank">{this.props.links[1].name}</a></h3>
+							<h4><span className="label label-info">Travel</span></h4>
+						</div>
+					</div>
+			      </Col>
+			    </Row>
+			    <Row className="show-grid">
+			      <Col md={4}>
+			      	<div className="item">
+						<img className="logo" src="http://i.cdn.cnn.com/cnn/.e/img/3.0/global/misc/cnn-logo.png" alt={this.props.links[2].name} />
+						<div className="weblink">
+							<h3><a href={this.props.links[2].url} target="_blank">{this.props.links[2].name}</a></h3>
+							<h4><span className="label label-warning">News</span></h4>
+						</div>
+					</div>
+			      </Col>
+			    </Row>
+			    <Row className="show-grid">
+			      <Col md={4}>
+			      	<div className="item">
+						<img className="logo" src="http://i0.kym-cdn.com/entries/icons/original/000/004/562/maxresdefault.jpg" alt={this.props.links[3].name} />
+						<div className="weblink">
+							<h3><a href={this.props.links[3].url} target="_blank">{this.props.links[3].name}</a></h3>
+							<h4><span className="label label-danger">Fun</span></h4>
+							<h4><span className="label label-primary">Video</span></h4>
+						</div>
+					</div>
+			      </Col>
+			    </Row>
+			    
 
-			<div className="item">
-				<img className="logo" src="http://kintudesigns.com/wp-content/uploads/2016/11/angularjs.jpg" alt="angularjs" />
-				<div className="weblink">
-				<h3><a href="https://angularjs.org/" target="_blank">Angular</a></h3>
-				<h4><span className="label label-success">Web</span></h4>
-				</div>
-			</div>
+			    
+			
 
+			
 			</div>
 		);
 	}
